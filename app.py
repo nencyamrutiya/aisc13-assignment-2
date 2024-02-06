c768f394d9cb767cebb4d6186e1224160fb5fe0b
nencyamrutiya <139144776+nencyamrutiya@users.noreply.github.com>
2024-02-06 10:42:14 -0500

    fixed all the tests

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-print("Hello world")
-
-
 a = [1,2,3,4]
 for idx, item in enumerate(a):
     print(item)
 
-
 def is_odd_even(a):
     return a % 2 == 0
 
-
 # function tests
-assert(2) == True
-assert(2) == False
+# assert is_odd_even(2) == True
+# assert is_odd_even(3) == False
+
+def test_func():
+    assert 1+1 == 2
+    assert is_odd_even(2) == True
+    assert is_odd_even(3) == False
+    assert is_odd_even(10) != False
